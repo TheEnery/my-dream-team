f8443d38134ebbf9251a274134a6baa046eb31e2
Naz <klymok.nazariy@gmail.com>
2023-12-06 11:38:44 +0200

    [feat]: add card for Nazarii Klymok (#3)
    Co-authored-by: naz <nazarii.klymok@botscrew.com>

--- my-dream-team/src/app/app.component.ts
+++ my-dream-team/src/app/app.component.ts
@@ -3,15 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   members = [
+    {
+      name: 'Nazarii Klymok',
+      position: 'Design Director',
+      image: 'https://i.kym-cdn.com/photos/images/original/002/342/992/8a0.png',
+    },
     {
       name: "Kozyra Oleksandr",
       position: "Quality Assurance",
       image: "https://media.giphy.com/media/JlpihM5xjBAkszY14v/giphy.gif"
     },
-  ]
+  ];
   title = 'my-dream-team';
 }
