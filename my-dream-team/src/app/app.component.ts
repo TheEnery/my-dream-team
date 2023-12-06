ce1c17a4516ba88fc802d45f034d8488b5308b95
naz <nazarii.klymok@botscrew.com>
2023-12-06 11:33:44 +0200

    [feat]: add card for Nazarii Klymok

--- my-dream-team/src/app/app.component.ts
+++ my-dream-team/src/app/app.component.ts
@@ -3,15 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   members = [
     {
-      name: "Member name",
-      position: "Member position",
-      image: "https://winaero.com/blog/wp-content/uploads/2019/11/Photos-new-icon.png"
+      name: 'Nazarii Klymok',
+      position: 'Design Director',
+      image: 'https://i.kym-cdn.com/photos/images/original/002/342/992/8a0.png',
     },
-  ]
+  ];
   title = 'my-dream-team';
 }
